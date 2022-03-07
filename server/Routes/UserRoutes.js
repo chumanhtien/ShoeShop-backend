352853f2da74a61235cf2a29c6929ae515386f3f
chumanhtien <tiencm194182@gmail.com>
2022-03-08 00:38:22 +0700

    Chap 17, 18: Redux and API Login, Register

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -1,6 +1,9 @@
+import e from "express";
 import express from "express";
 import asyncHandler from "express-async-handler"
+import protect from "../Middleware/AuthMiddleware.js";
 import User from "../Models/UserModel.js";
+import generateToken from "../utils/generateToken.js";
 
 const userRouter = express.Router();
 
@@ -17,7 +20,7 @@ userRouter.post(
                 name: user.name,
                 email: user.email,
                 isAdmin: user.isAdmin,
-                token: null,
+                token: generateToken(user._id),
                 createdAt: user.createdAt,
             });
         }
@@ -35,5 +38,63 @@ userRouter.post(
             }
         }
     })
-)
+);
+
+//REGISTER
+userRouter.post(
+    "/register",
+    asyncHandler(async (req, res) => {
+        const {name, email, password} = req.body;
+        const userExist = await User.findOne({email});
+
+        if(userExist) {
+            res.status(400);
+            throw new Error("User already exists");
+        }
+        
+        const user = await User.create({
+            name, 
+            email, 
+            password
+        });
+
+        if(user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email, 
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id),
+                // createdAt: user.createdAt,
+            })
+        }
+        else {
+            res.status(400);
+            throw new Error("Invalid USer Data")
+        }
+    })
+);
+
+//PROFILE
+userRouter.get(
+    "/profile",
+    protect,
+    asyncHandler(async (req, res) => {
+        const user = await User.findById(req.user._id);
+        if (user) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email, 
+                isAdmin: user.isAdmin,
+                // token: generateToken(user._id),
+                createdAt: user.createdAt,
+            })
+        } else {
+            res.status(404);
+            throw Error("User not found")
+        }
+    })
+);
+
 export default userRouter;
