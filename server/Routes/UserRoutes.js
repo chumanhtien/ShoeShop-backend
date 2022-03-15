647367a023ec8c9d7fb8270ecef3970a6722afe6
chumanhtien <tiencm194182@gmail.com>
2022-03-15 15:48:00 +0700

    commit some changes for Admin get users

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import asyncHandler from "express-async-handler"
-import protect from "../Middleware/AuthMiddleware.js";
+import {admin, protect} from "../Middleware/AuthMiddleware.js";
 import User from "../Models/UserModel.js";
 import generateToken from "../utils/generateToken.js";
 
@@ -124,4 +124,14 @@ userRouter.put(
     })
 );
 
+//GET ALL USERS ADMIN
+userRouter.get(
+    "/", 
+    protect, 
+    admin,
+    asyncHandler(async (req, res) => {
+        const users = await User.find({})
+        res.json(users);
+    })
+);
 export default userRouter;
