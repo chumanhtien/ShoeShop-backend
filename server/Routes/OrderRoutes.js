08f4e93ac9112ebd1d03519d93d7c659b0c3150e
chumanhtien <tiencm194182@gmail.com>
2022-03-17 00:26:47 +0700

    Product Get List, Delete, Create, Edit, Update

--- server/Routes/OrderRoutes.js
+++ server/Routes/OrderRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import asyncHandler from "express-async-handler"
-import {protect} from "../Middleware/AuthMiddleware.js";
+import {admin, protect} from "../Middleware/AuthMiddleware.js";
 import Order from "../Models/OrderModel.js"
 const orderRouter = express.Router();
 
@@ -95,4 +95,16 @@ orderRouter.get(
     })
         
 );
+
+//ADMIN GET ALL ORDERS
+orderRouter.get(
+    "/admin/all",
+    protect,
+    admin,
+    asyncHandler(async (req, res) => {
+        const orders = await Order.find({}).sort({_id: -1}).populate("user", "id name email");
+        res.json(orders);
+    })
+);
+
 export default orderRouter;
--- server/Routes/ProductRoutes.js
+++ server/Routes/ProductRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import Product from "../Models/ProductModel.js";
-import {protect} from "../Middleware/AuthMiddleware.js"
+import {admin, protect} from "../Middleware/AuthMiddleware.js"
 import { PAGE_SIZE } from "../constants/PageConstants.js";
 
 const productRoute = express.Router();
@@ -28,6 +28,18 @@ productRoute.get(
         res.json({products, page, pages: Math.ceil(count / pageSize)});
     })
 );
+
+//ADMIN GET ALL PRODUCTS WITHOUT SEARCH AND PEGINATION
+productRoute.get(
+    "/all", 
+    protect, 
+    admin, 
+    asyncHandler(async (req, res)=> {
+        const products = await Product.find({}).sort({_id: -1});
+        res.json(products);
+    })
+);
+
 
 //Lay 1 product theo id == GET SINGLE PRODUCT
 productRoute.get(
@@ -84,6 +96,81 @@ productRoute.post(
         } else {
             res.status(404);
             throw new Error("Product not found!");
+        }
+    })  
+);
+
+//DELETE PRODUCT
+productRoute.delete(
+    "/:id",
+    protect,
+    admin,
+    asyncHandler(async (req, res) => {
+        const product = await Product.findById(req.params.id);
+        if(product) {
+            await product.remove();
+            res.json({message: "Product has already been deleted"});
+        } else {
+            res.status(404);
+            throw new Error("Product not found!");
+        }
+    })  
+);
+
+
+//CREATE PRODUCT
+productRoute.post(
+    "/",
+    protect,
+    admin,
+    asyncHandler(async (req, res) => {
+        const {name, price, description, image, countInStock} = req.body;
+        const productExist = await Product.findOne({name});
+        if(productExist) {
+            res.status(400);
+            throw new Error("Product name already exist!");
+        } else {
+            const product = new Product({
+                name, 
+                price, 
+                description, 
+                image, 
+                countInStock,
+                user: req.user._id,
+            })
+            if (product) {
+                const createdproduct = await product.save();
+                res.status(201).json(createdproduct);
+            }
+            else {
+                res.status(400);
+                throw new Error("Invalid product data");
+            }
+            
+        }
+    })  
+);
+
+//EDIT/UPDATE PRODUCT
+productRoute.put(
+    "/:id",
+    protect,
+    admin,
+    asyncHandler(async (req, res) => {
+        const {name, price, description, image, countInStock} = req.body;
+        const product = await Product.findById(req.params.id);
+        if(product) {
+            product.name = name || product.name; 
+            product.price = price || product.price; 
+            product.description = description || product.description; 
+            product.image = image || product.image; 
+            product.countInStock = countInStock || product.countInStock;
+
+            const updatedProduct = await product.save();
+            res.status(201).json(updatedProduct);
+        } else {
+            res.status(404);
+            throw new Error("Product not found");
         }
     })  
 );
